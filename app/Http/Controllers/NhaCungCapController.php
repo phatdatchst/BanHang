a95269e2c5c7a2a064b8833414fd1c8447118968
PhungNguyen <ntphung1400321@gmail.com>
2017-12-24 16:21:18 +0700

    dsdsd

--- app/Http/Controllers/NhaCungCapController.php
+++ app/Http/Controllers/NhaCungCapController.php
@@ -3,7 +3,6 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-<<<<<<< HEAD
 use App\NhaCungCap;
 
 class NhaCungCapController extends Controller
@@ -26,18 +25,5 @@ class NhaCungCapController extends Controller
     }
     public function postThem(Request $request){
         echo $request->tenncc;
-=======
-
-class NhaCungCapController extends Controller
-{
-    public function getDanhSach(){
-        
-    }
-    public function getThem(){
-    
-    }
-    public function getSua(){
-    
->>>>>>> 9c34850271ef5f797731a571d9bcc8e53017bc13
     }
 }

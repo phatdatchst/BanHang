c85072ebd72bc0f920042a3785a5abc20daa0705
phatdatchst <lpdat.ktpm0114@student.ctuet.edu.vn>
2017-12-24 16:34:43 +0700

    dsadasdasd

--- app/Http/Controllers/NhaCungCapController.php
+++ app/Http/Controllers/NhaCungCapController.php
@@ -24,6 +24,31 @@ class NhaCungCapController extends Controller
         return view('admin.nhacungcap.them');
     }
     public function postThem(Request $request){
-        echo $request->tenncc;
-    }
+        $this->validate($request, [
+            'tenncc'=>'required|min:2|max:10',
+            'quocgia'=>'required|min:2:max:30',
+            'sdt'=>'required|min:2:max:30',
+            'diachi'=>'required|min:2:max:30',
+        ],[
+            'tenncc.required' =>'Bạn chưa nhập mã khách hàng',
+            'tenncc.min'=>'Mã khách hàng quá ngắn',
+            'tenncc.max'=>'Mã khách hàng quá dài',
+            'quocgia.required' =>'Bạn chưa nhập tên thành viên',
+            'quocgia.min'=>'Tên thành viên quá ngắn',
+            'quocgia.max'=>'Tên thành viên quá dài',
+            'diachi.required' =>'Bạn chưa nhập tài khoản',
+            'diachi.min'=>'Tài khoản quá ngắn',
+            'diachi.max'=>'Tài khoản quá dài',
+              
+        ]);
+        $nhacungcap = new NhaCungCap();
+        $nhacungcap->tenncc = $request->tenncc;
+        $nhacungcap->quocgia = $request->quocgia;
+        $nhacungcap->sdt = $request->sdt;
+        $nhacungcap->diachi = $request->diachi;
+        $nhacungcap->save();
+        
+        
+        return redirect('admin/nhacungcap/them')->with('thongbao','Thêm Thành Công');
+}
 }

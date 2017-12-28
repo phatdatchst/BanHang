805f0fc803d981955a77fa119a560c1b158060b0
PhungNguyen <ntphung1400321@gmail.com>
2017-12-28 15:57:31 +0700

    ok

--- routes/web.php
+++ routes/web.php
@@ -110,28 +110,6 @@ Route::get('chitietsanpham', function (){
 Route::get('dangki', function (){
     return view('pages.dangki');
 });
-Route::group(['prefix'=>'admin'], function (){
-    Route::group(['prefix'=>'nhacungcap'], function (){
-        Route::get('danhsach', 'NhaCungCapController@getDanhSach');
-        
-        Route::get('sua', 'NhaCungCapController@getSua');
-        
-        Route::get('them', 'NhaCungCapController@getThem');
-        Route::post('them','NhaCungCapController@postThem');
-    });
-        Route::group(['prefix'=>'nhanvien'], function (){
-            Route::get('danhsach', 'NhanVienController@getDanhSach');
-            Route::get('sua', 'NhanVienController@getSua');
-            Route::get('them', 'NhanVienController@getThem');
-        });
-        Route::group(['prefix'=>'sanpham'], function (){
-            Route::get('danhsach', 'SanPhamController@getDanhSach');
-            Route::get('sua', 'SanPhamController@getSua');
-            Route::get('them', 'SanPhamController@getThem');
-        });
-        Route::group(['prefix'=>'thanhvien'], function (){
-            Route::get('danhsach', 'ThanhVienController@getDanhSach');
-            Route::get('sua', 'ThanhVienController@getSua');
-            Route::get('them', 'ThanhVienController@getThem');
-        });
+Route::get('sanpham', function (){
+    return view('pages.sanpham');
 });
